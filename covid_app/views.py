51782cdbdac325448ec74d2cf72446bd5c76cf7d
Talha Jubair <talhajubair100.bd@gmail.com>
2021-01-18 17:06:25 +0600

    country select added and api include

--- covid_app/views.py
+++ covid_app/views.py
@@ -2,6 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 
+import requests
+
+url = "https://covid-193.p.rapidapi.com/statistics"
+
+headers = {
+    'x-rapidapi-key': "88b8319385msh1013fdd44af27bcp19bc49jsn733943950085",
+    'x-rapidapi-host': "covid-193.p.rapidapi.com"
+    }
+
+response = requests.request("GET", url, headers=headers).json()
+
+#print(response.text)
+
 
 def home(request):
-    return render(request, 'home.html')
+    noresults = response['results']
+    con_list = []
+    for i in range(0, noresults):
+        con_list.append(response['response'][i]['country'])
+    context = {'con_list': con_list}
+    return render(request, 'home.html', context)
