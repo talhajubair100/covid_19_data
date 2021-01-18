128b8acf0d4bb9b2c9b42707977bdefbcdef721c
Talha Jubair <talhajubair100.bd@gmail.com>
2021-01-18 21:06:50 +0600

    data show complete

--- covid_app/views.py
+++ covid_app/views.py
@@ -17,9 +17,29 @@ response = requests.request("GET", url, headers=headers).json()
 
 
 def home(request):
-    noresults = response['results']
+    noresults = int(response['results'])
     con_list = []
     for i in range(0, noresults):
         con_list.append(response['response'][i]['country'])
+
+    if request.method == "POST":
+        selectedcountry = request.POST['selectedcountry']
+        noresults = int(response['results'])
+
+        for i in range(0, noresults):
+            if selectedcountry == response['response'][i]['country']:
+                print('country', selectedcountry)
+                new = response['response'][i]['cases']['new']
+                active = response['response'][i]['cases']['active']
+                critical = response['response'][i]['cases']['critical']
+                recovered = response['response'][i]['cases']['recovered']
+                M_pop = response['response'][i]['cases']['1M_pop']
+                total = response['response'][i]['cases']['total']
+                deaths = int(total) - int(active)-int(recovered)
+        context = {'selectedcountry': selectedcountry, 'con_list': con_list, 'new': new, 'active': active, 'critical': critical, 'recovered': recovered, 'M_pop': M_pop, 'total': total, 'deaths': deaths}
+        return render(request, 'home.html', context)
+
+    
     context = {'con_list': con_list}
+    
     return render(request, 'home.html', context)
